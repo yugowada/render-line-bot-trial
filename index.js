21e7fc6d64531ff2303f9b76fb585605f6534b7b
yugowada <115605512+yugowada@users.noreply.github.com>
2023-06-25 21:14:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確�
 app.post('/webhook', line.middleware(config), (req, res) => {
     console.log(req.body.events);
 
-    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('疎通確認用');
-        return; 
-    }
 
     Promise
       .all(req.body.events.map(handleEvent))
